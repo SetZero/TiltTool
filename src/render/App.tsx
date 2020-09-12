ad2a581b584268d964a24f20d5412962e00a620a
Sebastian <19Sebastian95@gmx.de>
2020-09-12 14:55:10 +0200

    Added Initial TS + React Files

---
+++ src/render/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+    return (
+    <div className="App">
+        Test
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.rules.js
@@ -0,0 +1,29 @@
+module.exports = [
+  // Add support for native node modules
+  {
+    test: /\.tsx?$/,
+    exclude: /(node_modules|.webpack)/,
+    loaders: [{
+      loader: 'ts-loader',
+      options: {
+        transpileOnly: true
+      }
+    }]
+  }
+  // Put your webpack loader rules in this array.  This is where you would put
+  // your ts-loader configuration for instance:
+  /**
+   * Typescript Example:
+   *
+   * {
+   *   test: /\.tsx?$/,
+   *   exclude: /(node_modules|.webpack)/,
+   *   loaders: [{
+   *     loader: 'ts-loader',
+   *     options: {
+   *       transpileOnly: true
+   *     }
+   *   }]
+   * }
+   */
+];
